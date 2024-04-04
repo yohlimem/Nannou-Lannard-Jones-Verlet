5abeb9f403c352f23ea7246a568293b7878c40cb
Yahli Mariansky <yahlimem@gmail.com>
2024-04-04 20:25:15 +0300

    forces

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use nannou::prelude::*;
+use egui_plot::{Line, Plot, PlotPoints};
+use nannou::{color::rgb::Rgb, draw::theme::Color, prelude::*};
 use nannou_egui::{self, egui, Egui};
 use rand::Rng;
-use egui_plot::{Line, Plot, PlotPoints};
-
+mod points;
+use points::Points;
 /*sigma = 10 # nm
 epsilon = 500 # J
 K = 20 # temporary
@@ -14,127 +15,106 @@ p_l_copy = copy.deepcopy(p_l)
 struct Model {
     // window: Window,
     egui: Egui,
-    sigma: f32, // nm
-    epsilon: f32, // J
-    K: f32, // temporary
+    sigma: f32,             // nm
+    epsilon: f32,           // J
+    K: f32,                 // temporary
     plate_size: (f32, f32), // nm^2
-    p_l: Vec<(f32, f32)>, // list of all particles
-    p_l_copy: Vec<(f32, f32)>,
+    p_l: Vec<Points>,       // list of all particles
+    p_l_copy: Vec<Points>,
+    speed: f32,
 }
 
-fn gen_atoms(model: &mut Model, n: u32, seed_size: u32){
-    let mut l:Vec<(f32, f32)> = Vec::new();
+fn gen_atoms(model: &mut Model, n: u32, seed_size: u32) {
+    let mut l: Vec<Points> = Vec::new();
     let mut rng = rand::thread_rng();
-    for _ in 0..n{
-        let p = (rng.gen_range(0.0..model.plate_size.0 as f32), rng.gen_range(0.0..model.plate_size.1 as f32));
-        if p.0 < model.plate_size.0/2.0+((f32::sqrt(seed_size as f32) as i32-1)/2) as f32 * model.sigma*2.0 + 20.0 && p.0 > model.plate_size.0/2.0 - ((f32::sqrt(seed_size as f32) as i32)/2) as f32 * model.sigma*2.0 - 20.0 && p.1 < model.plate_size.1/2.0+((f32::sqrt(seed_size as f32) as i32-1)/2) as f32 * model.sigma*2.0 + 20.0 && p.1 > model.plate_size.1/2.0 - ((f32::sqrt(seed_size as f32) as i32)/2) as f32 * model.sigma*2.0 - 20.0 {
-            continue;
-        }
+    for i in 0..n {
+        let x = rng.gen_range(0.0..model.plate_size.0);
+        let y = rng.gen_range(0.0..model.plate_size.1);
+        let p = Points::new(
+            vec2(
+                x,
+                y,
+            ),
+            vec2(0.0, 0.0),
+            vec2(0.0, 0.0),
+            model.plate_size,
+            i as f32%2.0  - 0.4,
+        );
+        // if p.pos.x
+        //     < model.plate_size.0 / 2.0
+        //         + ((f32::sqrt(seed_size as f32) as i32 - 1) / 2) as f32 * model.sigma * 2.0
+        //         + 20.0
+        //     && p.pos.x
+        //         > model.plate_size.0 / 2.0
+        //             - ((f32::sqrt(seed_size as f32) as i32) / 2) as f32 * model.sigma * 2.0
+        //             - 20.0
+        //     && p.pos.y
+        //         < model.plate_size.1 / 2.0
+        //             + ((f32::sqrt(seed_size as f32) as i32 - 1) / 2) as f32 * model.sigma * 2.0
+        //             + 20.0
+        //     && p.pos.y
+        //         > model.plate_size.1 / 2.0
+        //             - ((f32::sqrt(seed_size as f32) as i32) / 2) as f32 * model.sigma * 2.0
+        //             - 20.0
+        // {
+        //     continue;
+        // }
         l.push(p);
     }
-    for x in 0..(f32::sqrt(seed_size as f32) as u32){
-        for y in 0..(f32::sqrt(seed_size as f32) as u32){
-            let new_x = model.plate_size.0 / 2.0 + (x as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 2.0;
-            let new_y = model.plate_size.1 / 2.0 + (y as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 2.0;
-
-            l.push((new_x, new_y))
+    for x in 0..(f32::sqrt(seed_size as f32) as u32) {
+        for y in 0..(f32::sqrt(seed_size as f32) as u32) {
+            let new_x = model.plate_size.0 / 2.0 + (x as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 0.3;
+            let new_y = model.plate_size.1 / 2.0 + (y as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 0.3;
+            println!("{}", (x + y) as f32%2.0);
+            // l.push(Points::new(vec2(new_x, new_y), vec2(0.0, 0.0), vec2(0.0, 0.0), model.plate_size, (x + y) as f32%2.0 - 0.4));
+            l.push(Points::new(vec2(new_x, new_y), vec2(0.0, 0.0), vec2(0.0, 0.0), model.plate_size, (x + y) as f32%2.0 - 0.4));
         }
     }
     model.p_l = l.clone();
     model.p_l_copy = l;
-    // return l;
-    // p_l = gen_atoms(200, 50);
-}
-fn u(model: &Model, r: f32) -> f32 { // potential energy
-    return 4.0 * model.epsilon * ((model.sigma / r).powi(12) - (model.sigma / r).powi(6));
 }
 
-fn distance(model: &Model, i: usize, j: usize) -> f32 {
-    let p_i = model.p_l[i];
-    let p_j = model.p_l[j];
-    ((p_j.0 - p_i.0).powi(2) + (p_j.1 - p_i.1).powi(2)).sqrt()
-}
-
-fn distance_pos(p: (f32, f32), p2: (f32, f32)) -> f32 {
-    ((p2.0 - p.0).powi(2) + (p2.1 - p.1).powi(2)).sqrt()
-}
-
-fn energy(model: &Model, i: usize) -> f32 {
-    let mut potential = 0.0;
-    for j in 0..model.p_l.len() {
-        if j == i {
-            continue;
-        }
-        potential += u(&model, distance(model, i, j));
+fn simulation_step(p_l_copy: &mut Vec<Points>, p_l: &mut Vec<Points>) {
+    // let mut new_p_l_copy = p_l.clone();
+    *p_l_copy = p_l.clone();
+    
+    for p in p_l_copy.iter_mut() {
+        p.step(p_l);
+        // println!("step")
     }
-    potential
-}
-
-fn energy_pos(model: &Model, p: (f32, f32), index: usize) -> f32 {
-    let mut potential = 0.0;
-    for (i, p2) in model.p_l.iter().enumerate() {
-        if i == index {
-            continue;
-        }
-        potential += u(&model, distance_pos(p, *p2));
-    }
-    potential
-}
-
-fn choose_delta(model: &Model, i: usize) -> Option<(f32, f32)> {
-    let pos = model.p_l[i];
-    for _ in 0..10 {
-        let dy = rand::thread_rng().gen_range(-model.K..model.K);
-        let dx = rand::thread_rng().gen_range(-model.K..model.K);
-        if pos.0 + dx < model.plate_size.0 && pos.0 + dx > 0.0 && pos.1 + dy > 0.0 || pos.1 + dx < model.plate_size.1 {
-            return Some((pos.0 + dx, pos.1 + dy));
-        }
-    }
-    None
-}
-
-fn step(model: &mut Model, i: usize) {
-    if let Some(new_pos) = choose_delta(model, i) {
-        if energy_pos(model, new_pos, i) < energy(model, i) {
-            model.p_l_copy[i] = new_pos;
-        }
-    }
-}
-
-fn simulation(model: &mut Model) {
-    for i in 0..model.p_l.len() {
-        step(model, i);
-    }
-    model.p_l = model.p_l_copy.clone();
-}
-
-// fn simulation_loop(model: &mut Model, n_iter: usize) {
-//     for _ in 0..n_iter {
-//         simulation(model);
-//     }
-// }
-
-fn main() {
-    nannou::app(model).update(update).run();
+    
+    *p_l = p_l_copy.clone();
     
 }
 
+
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
 fn model(app: &App) -> Model {
-    let window_id = app.new_window().view(view).raw_event(raw_window_event).build().unwrap();
+    let window_id = app
+        .new_window()
+        .view(view)
+        .raw_event(raw_window_event)
+        .build()
+        .unwrap();
     let window = app.window(window_id).unwrap();
     let egui = Egui::from_window(&window);
     let num = 0.0;
-    let p_l = vec![(100.0, 100.0), (60.0, 35.0), (20.0, 200.0), (300.0, 300.0)];
+    // let p_l: Vec<Points> = p_l.into_iter().map(|(x, y)| Points::new(vec2(x, y), vec2(0.0, 0.0), vec2(0.0, 0.0))).collect();
     let mut model = Model {
         egui,
-        sigma: 10.0,
+        sigma: 100.0,
         epsilon: 500.0,
         K: 20.0,
         plate_size: (500.0, 500.0),
-        p_l_copy: p_l.clone(),
-        p_l,
+        p_l_copy: vec![],
+        p_l: vec![],
+        speed: 1.0,
     };
-    gen_atoms(&mut model, 100, 20);
+        gen_atoms(&mut model, 100, 50);
     // println!("{:?}", model.p_l);
     model
 }
@@ -147,7 +127,6 @@ fn update(app: &App, model: &mut Model, update: Update) {
 
         let ctx = egui.begin_frame();
 
-
         egui::Window::new("Simulation controls").show(&ctx, |ui| {
             ui.label("step_size");
             ui.add(egui::Slider::new(&mut model.K, 1.0..=200.0));
@@ -155,24 +134,23 @@ fn update(app: &App, model: &mut Model, update: Update) {
             ui.add(egui::Slider::new(&mut model.sigma, 1.0..=100.0));
             ui.label("epsilon");
             ui.add(egui::Slider::new(&mut model.epsilon, 1.0..=1000.0));
-            ui.label("Restart simulation");
+            ui.label("speed");
+            ui.add(egui::Slider::new(&mut model.speed, 1.0..=100.0));
+            // speed
 
-            
             let button = ui.button("Restart");
-            button_clicked = button.clicked()
+            button_clicked = button.clicked();
         });
-
     }
     if button_clicked {
-        gen_atoms(model, 100, 20);
+        gen_atoms(model, 100, 50);
     }
-
-    simulation(&mut *model);
-
-
+    for _ in 0..model.speed as u32 {
+        simulation_step(&mut model.p_l_copy, &mut model.p_l);
+    }
 }
 
-fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent){
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
     model.egui.handle_raw_event(event);
 }
 
@@ -182,41 +160,50 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // get app window size
     let size = app.window_rect();
 
-
     // draw circles
     for p in &model.p_l {
-        draw.ellipse().x_y(p.0 - model.plate_size.0 / 2.0, p.1 - model.plate_size.1 / 2.0).radius(5.0).color(BLACK);
+        draw.ellipse()
+            .x_y(
+                p.pos.x - model.plate_size.0 / 2.0,
+                p.pos.y - model.plate_size.1 / 2.0,
+            )
+            .radius(5.0)
+            .color(Rgba::new(1.0, p.charge,0.0 , 1.0));
     }
 
-    draw.rect().w_h(size.w(), size.h()).x_y(size.w()/1.2, size.h()/1.5).stroke_color(BLACK).stroke_weight(1.0).no_fill();
-    draw_graph(&draw, app, &model);
+    draw.rect()
+        .w_h(size.w(), size.h())
+        .x_y(size.w() / 1.2, size.h() / 1.5)
+        .stroke_color(BLACK)
+        .stroke_weight(1.0)
+        .no_fill();
+    // draw_graph(&draw, app, &model);
 
-    
     draw.to_frame(app, &frame).unwrap();
     model.egui.draw_to_frame(&frame).unwrap();
 }
 
-fn draw_graph(draw: &Draw, app: &App, model: &Model){
-    let size = app.window_rect();
-    let graph_x = size.w() / 3.0;
-    let graph_y = size.h() / 3.0;
-    // let graph_x = 0.0;
-    // let graph_y = 0.0;
-    
-    let size = vec2(3.0, 0.1);
+// fn draw_graph(draw: &Draw, app: &App, model: &Model) {
+//     let size = app.window_rect();
+//     let graph_x = size.w() / 3.0;
+//     let graph_y = size.h() / 3.0;
+//     // let graph_x = 0.0;
+//     // let graph_y = 0.0;
 
-    for r in -5..1000{
-        if r == 0 || r == 1{
-            continue;
-        }
-        let start_x = (r as f32 - 1.0) * size.x + graph_x;
-        let start_y = u(&model, r as f32 - 1.0) * size.y + graph_y;
-        let end_x = r as f32 * size.x + graph_x;
-        let end_y = u(&model, r as f32) * size.y + graph_y;
+//     let size = vec2(3.0, 0.1);
 
-        draw.line()
-            .start(pt2(start_x, start_y))
-            .end(pt2(end_x, end_y))
-            .color(BLACK);
-    }
-}
+//     for r in -5..1000 {
+//         if r == 0 || r == 1 {
+//             continue;
+//         }
+//         let start_x = (r as f32 - 1.0) * size.x + graph_x;
+//         let start_y = u(&model, r as f32 - 1.0) * size.y + graph_y;
+//         let end_x = r as f32 * size.x + graph_x;
+//         let end_y = u(&model, r as f32) * size.y + graph_y;
+
+//         draw.line()
+//             .start(pt2(start_x, start_y))
+//             .end(pt2(end_x, end_y))
+//             .color(BLACK);
+//     }
+// }
---
+++ src/points.rs
@@ -0,0 +1,110 @@
+use nannou::prelude::*;
+
+use crate::Model;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Points {
+    pub pos: Vec2,
+    pub v: Vec2,
+    pub a: Vec2,
+    pub mass: f32,
+    pub charge: f32,
+    pub plate_size: (f32, f32)
+}
+
+impl Points {
+    pub fn new(x0: Vec2, v0: Vec2, a: Vec2, plate_size: (f32, f32), charge: f32) -> Self {
+        Points {
+            pos: x0,
+            v: v0,
+            a,
+            mass: 1.0,
+            plate_size,
+            charge,
+        }
+    }
+
+    pub fn random_init(plate_size: Vec2, charge: f32) -> Self {
+        let x = vec2(
+            random_range(0.0, plate_size.x),
+            random_range(0.0, plate_size.y),
+        );
+        let v = vec2(0.0, 0.0);
+        let a = vec2(0.0, 0.0);
+
+        Points {
+            pos: x,
+            v,
+            a,
+            mass: 1.0,
+            plate_size: (plate_size.x, plate_size.y),
+            charge,
+        }
+    }
+
+    pub fn solver(&mut self, dt: f32) {
+        self.v += self.a * dt;
+        self.pos += self.v * dt;
+        if self.pos.x > self.plate_size.0 || self.pos.x < 0.0 {
+            self.v.x *= -1.0;
+            // self.pos += self.v * dt;
+        }
+
+        if self.pos.y > self.plate_size.1 || self.pos.y < 0.0 {
+            self.v.y *= -1.0;
+            // self.pos += self.v * dt;
+        }
+        self.a = vec2(0.0, 0.0);
+    }
+
+    fn force(&self, r: f32) -> f32 {
+        let epsilon = 1000.0; // replace with actual value
+        let sigma = 10.0; // replace with actual value
+
+        let mut r2 = 0.0;
+        if r > sigma*1.07{
+            r2 = r
+        }
+        else{
+            r2 = sigma*1.07
+        }
+
+        -4.0 * epsilon * ((12.0 * (sigma.powi(12)) / r2.powi(13)) - (6.0 * (sigma.powi(6)) / r2.powi(7)))
+    }
+    fn charge_force(&self, p: &Points, r: f32) -> f32 {
+        let k = 100.0;
+        if r > 100.0 {
+            return 0.0;
+        }
+        if r < 1.0 {
+            return  k * -self.charge * p.charge / 1.0;
+        }
+        k * -self.charge * p.charge / (r*r)
+    }
+
+    pub fn dist_to(&self, p: &Points) -> f32 {
+        self.pos.distance(p.pos)
+    }
+
+    pub fn r_vector(&self, p: &Points) -> Vec2 {
+        (p.pos - self.pos).normalize()
+    }
+
+    pub fn step(&mut self, p_l: &[Points]) {
+        for p in p_l {
+            if p.pos == self.pos {
+                continue;
+            }
+            let r = self.dist_to(p);
+            if r > 100.0 {
+                continue;
+            }
+            self.a += (self.force(r) / self.mass) * self.r_vector(p);
+            self.a += (self.charge_force(p, r) / self.mass) * self.r_vector(p);
+        }
+        self.v *= 0.9992;
+        // self.a -= self.a.normalize();
+
+        self.solver(0.01);
+    }
+}
