37e94fb672888bb3d767e0be2decd8241f982ec9
Yahli Mariansky <yahlimem@gmail.com>
2024-05-09 21:35:09 +0300

    VERLET MY SAVIOR SO SKIBIDI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use egui_plot::{Line, Plot, PlotPoints};
-use nannou::{color::rgb::Rgb, draw::theme::Color, prelude::*};
-use nannou_egui::{self, egui, Egui};
+use nannou::{color::rgb::Rgb, draw::theme::Color, prelude::*, text::rt::Point};
+use nannou_egui::{self, egui::{self, Response}, Egui};
 use rand::Rng;
 mod points;
 use points::Points;
@@ -22,49 +22,50 @@ struct Model {
     p_l: Vec<Points>,       // list of all particles
     p_l_copy: Vec<Points>,
     speed: f32,
+    time: f32,
+    done_simulation: bool,
+    total_force: f32,
+    seed_size: u32,
+    atom_count: u32,
 }
 
 fn gen_atoms(model: &mut Model, n: u32, seed_size: u32) {
     let mut l: Vec<Points> = Vec::new();
     let mut rng = rand::thread_rng();
+
+    let seed_x0 = model.plate_size.0 / 2.0 + (0.0 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * 1.12246204831 * model.sigma;
+    let seed_y0 = model.plate_size.1 / 2.0 + (0.0 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * 1.12246204831 * model.sigma;
+    let seed_x1 = model.plate_size.0 / 2.0 + (f32::sqrt(seed_size as f32) - 1.0) / 2.0 * 1.12246204831 * model.sigma;
+    let seed_y1 = model.plate_size.1 / 2.0 + (f32::sqrt(seed_size as f32) - 1.0) / 2.0 * 1.12246204831 * model.sigma;
     for i in 0..n {
         let x = rng.gen_range(0.0..model.plate_size.0);
         let y = rng.gen_range(0.0..model.plate_size.1);
+        if !(x < seed_x0 - 20.0 || x > seed_x1 + 20.0) && !(y < seed_y0 - 20.0 || y > seed_y1 + 20.0) {
+            continue;
+        }
+
+
         let p = Points::new(
             vec2(
                 x,
                 y,
             ),
-            vec2(0.0, 0.0),
+            -vec2(
+                x,
+                y,
+            ),
             vec2(0.0, 0.0),
             model.plate_size,
             i as f32%2.0  - 0.4,
         );
-        // if p.pos.x
-        //     < model.plate_size.0 / 2.0
-        //         + ((f32::sqrt(seed_size as f32) as i32 - 1) / 2) as f32 * model.sigma * 2.0
-        //         + 20.0
-        //     && p.pos.x
-        //         > model.plate_size.0 / 2.0
-        //             - ((f32::sqrt(seed_size as f32) as i32) / 2) as f32 * model.sigma * 2.0
-        //             - 20.0
-        //     && p.pos.y
-        //         < model.plate_size.1 / 2.0
-        //             + ((f32::sqrt(seed_size as f32) as i32 - 1) / 2) as f32 * model.sigma * 2.0
-        //             + 20.0
-        //     && p.pos.y
-        //         > model.plate_size.1 / 2.0
-        //             - ((f32::sqrt(seed_size as f32) as i32) / 2) as f32 * model.sigma * 2.0
-        //             - 20.0
-        // {
-        //     continue;
-        // }
+
+
         l.push(p);
     }
     for x in 0..(f32::sqrt(seed_size as f32) as u32) {
         for y in 0..(f32::sqrt(seed_size as f32) as u32) {
-            let new_x = model.plate_size.0 / 2.0 + (x as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 0.3;
-            let new_y = model.plate_size.1 / 2.0 + (y as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * model.sigma * 0.3;
+            let new_x = model.plate_size.0 / 2.0 + (x as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * 1.12246204831 * model.sigma;
+            let new_y = model.plate_size.1 / 2.0 + (y as f32 - ((seed_size as f32).sqrt() - 1.0) / 2.0) as f32 * 1.12246204831 * model.sigma;
             // println!("{}", (x + y) as f32%2.0);
             // l.push(Points::new(vec2(new_x, new_y), vec2(0.0, 0.0), vec2(0.0, 0.0), model.plate_size, (x + y) as f32%2.0 - 0.4));
             l.push(Points::new(vec2(new_x, new_y), vec2(0.0, 0.0), vec2(0.0, 0.0), model.plate_size, (x + y) as f32%2.0 - 0.4));
@@ -75,14 +76,21 @@ fn gen_atoms(model: &mut Model, n: u32, seed_size: u32) {
     model.p_l_copy = l;
 }
 
-fn simulation_step(p_l_copy: &mut Vec<Points>, p_l: &mut Vec<Points>) {
-    // let mut new_p_l_copy = p_l.clone();
+fn simulation_step(p_l_copy: &mut Vec<Points>, p_l: &mut Vec<Points>, app: &App, simulation_stop: &mut bool, model_total_force: &mut f32, epsilon: f32, sigma: f32) {
+
     *p_l_copy = p_l.clone();
-    
-    for p in p_l_copy.iter_mut() {
-        p.step(p_l);
-        // println!("step")
+
+    let mut total_force = 0.0;
+    if *simulation_stop {
+        return;
     }
+    for (i, p) in p_l_copy.iter_mut().enumerate() {
+        // println!("{}", p.pos);
+
+        total_force += p.step(p_l, epsilon, sigma);
+        // println!("{total_potential}")
+    }
+    *model_total_force = total_force;
     
     *p_l = p_l_copy.clone();
     
@@ -104,24 +112,34 @@ fn model(app: &App) -> Model {
     let window = app.window(window_id).unwrap();
     let egui = Egui::from_window(&window);
     let num = 0.0;
+    let seed_size = 100;
+    let atom_count = 100;
     // let p_l: Vec<Points> = p_l.into_iter().map(|(x, y)| Points::new(vec2(x, y), vec2(0.0, 0.0), vec2(0.0, 0.0))).collect();
     let mut model = Model {
         egui,
-        sigma: 100.0,
-        epsilon: 500.0,
+        sigma: 10.0,
+        epsilon: 10.0,
         K: 20.0,
-        plate_size: (500.0, 500.0),
+        plate_size: (300.0, 300.0),
         p_l_copy: vec![],
         p_l: vec![],
         speed: 1.0,
+        time: 0.0,
+        done_simulation: false,
+        total_force: 0.0,
+        seed_size,
+        atom_count,
     };
-        gen_atoms(&mut model, 100, 50);
+    gen_atoms(&mut model, atom_count, seed_size);
+
     // println!("{:?}", model.p_l);
     model
 }
 
 fn update(app: &App, model: &mut Model, update: Update) {
     let mut button_clicked = false;
+    let mut slider_seed: Option<Response> = Option::None;
+    let mut slider_atom: Option<Response> = Option::None;
     {
         let egui = &mut model.egui;
         egui.set_elapsed_time(update.since_start);
@@ -129,25 +147,62 @@ fn update(app: &App, model: &mut Model, update: Update) {
         let ctx = egui.begin_frame();
 
         egui::Window::new("Simulation controls").show(&ctx, |ui| {
-            // ui.label("step_size");
-            // ui.add(egui::Slider::new(&mut model.K, 1.0..=200.0));
-            // ui.label("sigma/distance");
-            // ui.add(egui::Slider::new(&mut model.sigma, 1.0..=100.0));
-            // ui.label("epsilon");
-            // ui.add(egui::Slider::new(&mut model.epsilon, 1.0..=1000.0));
+            ui.label("step_size");
+            ui.add(egui::Slider::new(&mut model.K, 1.0..=200.0));
+            ui.label("sigma/distance");
+            ui.add(egui::Slider::new(&mut model.sigma, 1.0..=100.0));
+            ui.label("epsilon");
+            ui.add(egui::Slider::new(&mut model.epsilon, 1.0..=1000.0));
             ui.label("speed");
-            ui.add(egui::Slider::new(&mut model.speed, 1.0..=100.0));
-            // speed
+            ui.add(egui::Slider::new(&mut model.speed, 1.0..=1000.0));
+            ui.label("start control");
+            ui.label("seed size");
+            slider_seed = Some(ui.add(egui::Slider::new(&mut model.seed_size, 1..=500)));
+            ui.label("atom count");
+            slider_atom = Some(ui.add(egui::Slider::new(&mut model.atom_count, 1..=500)));
 
+            
+            ui.label("time");
+            ui.label(model.time.to_string());
+            ui.label("total potential");
+            ui.label(model.total_force.to_string());
+
+            let stop = ui.button("Stop");
+            if stop.clicked() {
+                model.done_simulation = !model.done_simulation;
+                println!("stop: {}", model.done_simulation);
+            }
+            let step = ui.button("Step");
+            if step.clicked() {
+                    for _ in 0..model.speed as u32 {
+                        simulation_step(&mut model.p_l_copy, &mut model.p_l, app, &mut false, &mut model.total_force, model.epsilon, model.sigma);
+                        model.time += 0.01*0.1;
+                    }
+            }
+
+            
             let button = ui.button("Restart");
             button_clicked = button.clicked();
         });
     }
-    if button_clicked {
-        gen_atoms(model, 100, 50);
+    if slider_seed.unwrap().changed() {
+        gen_atoms(model, model.atom_count, model.seed_size);
     }
+    if slider_atom.unwrap().changed() {
+        gen_atoms(model, model.atom_count, model.seed_size);
+    }
+    if button_clicked {
+        gen_atoms(model, model.atom_count, model.seed_size);
+    }
+    // if model.done_simulation {
+    //     return;
+    // }
     for _ in 0..model.speed as u32 {
-        simulation_step(&mut model.p_l_copy, &mut model.p_l);
+        simulation_step(&mut model.p_l_copy, &mut model.p_l, app, &mut model.done_simulation, &mut model.total_force, model.epsilon, model.sigma);
+        if !model.done_simulation {
+            model.time += 0.01*0.1;
+            // break;
+        }
     }
 }
 
@@ -161,8 +216,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // get app window size
     let size = app.window_rect();
 
+
     // draw circles
     for p in &model.p_l {
+        // println!("p_l: {:?}", p);
+
         draw.ellipse()
             .x_y(
                 p.pos.x - model.plate_size.0 / 2.0,
--- src/points.rs
+++ src/points.rs
@@ -5,17 +5,20 @@ use nannou::prelude::*;
 #[derive(Debug, Clone, Copy)]
 pub struct Points {
     pub pos: Vec2,
+    pub last_pos: Vec2,
     pub v: Vec2,
     pub a: Vec2,
     pub mass: f32,
     pub charge: f32,
+    pub potential: f32,
     pub plate_size: (f32, f32)
 }
 
 impl Points {
-    const SIGMA:f32 = 10.0;
-    const EPSILON:f32 = 100.0;
+    //pub const SIGMA:f32 = 10.0;
+    //pub const EPSILON:f32 = 10.0;
     pub fn new(x0: Vec2, v0: Vec2, a: Vec2, plate_size: (f32, f32), charge: f32) -> Self {
+        // println!("{:?}", Self::SIGMA);
         Points {
             pos: x0,
             v: v0,
@@ -23,6 +26,8 @@ impl Points {
             mass: 1.0,
             plate_size,
             charge,
+            potential: 0.0,
+            last_pos: x0,
         }
     }
 
@@ -41,12 +46,16 @@ impl Points {
             mass: 1.0,
             plate_size: (plate_size.x, plate_size.y),
             charge,
+            last_pos: x,
+            potential: 0.0,
         }
     }
 
     pub fn solver(&mut self, dt: f32) {
-        self.v += self.a * dt;
-        self.pos += self.v * dt;
+        // self.v += self.a * dt;
+        self.pos += self.pos - self.last_pos + self.a * dt * dt;
+        self.last_pos = self.pos;
+        // println!("{:?}", self.pos);
         if self.pos.x > self.plate_size.0 || self.pos.x < 0.0 {
             self.v.x *= -1.0;
             // self.pos += self.v * dt;
@@ -59,72 +68,109 @@ impl Points {
         self.a = vec2(0.0, 0.0);
     }
 
-    fn force(&self, r: f32) -> f32 {
+    fn force(&self, r: f32, epsilon: f32, sigma: f32) -> f32 {
 
-        let mut r2 = 0.0;
-        if r > Self::SIGMA*1.07{
-            r2 = r
-        }
-        else if r < Self::SIGMA*0.1 {
-            r2 = Self::SIGMA*0.9
-        }
-        else if r < Self::SIGMA*1.07 {
-            r2 = Self::SIGMA*1.07
+        let r2 = r;
+        // if r > Self::SIGMA*1.07{
+        //     r2 = r
+        // }
+        // else if r < 0.1{
+        //     r2 = Self::SIGMA*100.0;
+        // }
+        // else if r < Self::SIGMA*0.1 {
+        //     r2 = Self::SIGMA*2.0
+        // }
+        // else if r < Self::SIGMA*1.07 {
+        //     r2 = Self::SIGMA*1.07
+        // }
+
+        if r2.powi(13) <= 0.001 {
+            println!("die");
         }
 
-        -4.0 * Self::EPSILON * ((12.0 * (Self::SIGMA.powi(12)) / r2.powi(13)) - (6.0 * (Self::SIGMA.powi(6)) / r2.powi(7)))
+
+        let f = -2.0 * epsilon * ((12.0 * (sigma.powi(12)) / r2.powi(13)) - (6.0 * (sigma.powi(6)) / r2.powi(7)));
+        if f.abs() > 1000.0 {
+            println!("f: {}", f);
+            println!("A1: {}", (12.0 * (sigma.powi(12))) / r2.powi(13));
+            println!("A2: {}", (6.0 * (sigma.powi(6)) / r2.powi(7)));
+        }
+        f
     }
+
+    fn u(&self, r: f32) -> f32 {
+        // let mut r2 = r;
+        // if r > Self::SIGMA*1.07{
+        //     r2 = r
+        // }
+        // else if r < Self::SIGMA*0.1 {
+        //     r2 = Self::SIGMA*2.0
+        // }
+        // else if r < Self::SIGMA*1.07 {
+        //     r2 = Self::SIGMA*1.07
+        // }
+        //2.0 * Self::EPSILON * ((Self::SIGMA.powi(12) / r.powi(12)) - (Self::SIGMA.powi(6) / r.powi(6)))
+        0.0
+    }
+
     fn charge_force(&self, p: &Points, r: f32) -> f32 {
-        let k = 30.0*Self::EPSILON;
-        if r < 1.0 {
-            return  (k * -self.charge * p.charge) / 1.0;
-        }
+        let k = 10.0 * 1000.0;
+        // if r < 1.0 {
+        //     return  (k * -self.charge * p.charge) / 1.0;
+        // }
         (k * -self.charge * p.charge) / (r*r)
     }
 
     pub fn dist_to(&self, p: &Points) -> f32 {
-        self.pos.distance(p.pos)
+        let dist = self.pos.distance(p.pos);
+        if dist == 0.0 {
+            return 0.01;
+        }
+        dist
+    }
+    pub fn dist_to_squared(&self, p: &Points) -> f32 {
+        self.pos.distance_squared(p.pos)
     }
 
     pub fn r_vector(&self, p: &Points) -> Vec2 {
-        (p.pos - self.pos).normalize()
+        let r = (p.pos - self.pos).normalize();
+        if r.is_nan() {
+            return vec2(0.0, 0.0);
+        }
+        r
+        
     }
 
-    pub fn step(&mut self, p_l: &[Points]) {
+    pub fn step(&mut self, p_l: &[Points], epsilon: f32, sigma: f32) -> f32 {
         for p in p_l {
             if p.pos == self.pos {
                 continue;
             }
             let r = self.dist_to(p);
-            if r > 300.0 {
+            if r > 100.0 {
                 continue;
             }
+            let force = self.force(r, epsilon, sigma);
             let r_vec = self.r_vector(p);
-            let new_a = (self.force(r) / self.mass) * r_vec + (self.charge_force(p, r) / self.mass) * r_vec;
-            self.a += new_a;
-            // if force.abs() > 1000.0 {
-            //     println!("force: {}", force);
-            // }
-            // if charge_force.abs() > 1000.0{
+            let new_a = (force / self.mass) * r_vec + (self.charge_force(p, r) / self.mass) * r_vec;
+            // self.potential += self.u(r);
+            // print!("{}", self.potential);
+            if r_vec.is_nan() {
+                println!("r_vec: {}", r_vec);
+            }
+            if force.is_nan() {
+                println!("force: nan");
+            }
+            // if self.charge_force(p, r).is_nan() {
             //     println!("charge: {}", self.charge)
             // }
-            // if force + charge_force > 1000.0 {
-            //     println!("force + charge: {}", force + charge_force);
-            // }
             // if new_a.length() > 1000.0 {
-            //     println!("r: {}", r);
-            //     println!("force: {}", force);
-            //     println!("charge: {}", self.charge);
-
-            //     println!("new a: {}", new_a);
+                
             // }
+            self.a += new_a;
         }
-        // if self.a.length() > 1000.0 {
-        //     println!("a: {}", self.a);
-        // }
-        self.v *= 0.9992;
-        // self.a -= self.a.normalize();
-
-        self.solver(0.01);
+        let total_force = self.a.length() * self.mass;
+        self.solver(0.1);
+        return total_force;
     }
 }
